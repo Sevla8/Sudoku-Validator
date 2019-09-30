db0c37df8bf17c69a0efc3acbb7e89410e902ddd
Da Silva Alves <ed191038@c39393l.ens.uqam.ca>
2019-09-30 19:21:58 -0400

    add reader

--- sudoku.c
+++ sudoku.c
@@ -3,15 +3,18 @@
 #include <pthread.h>
 #include <unistd.h>
 
-int sudoku[9][9] = {{6, 2, 4, 5, 3, 9, 1, 8, 7}, 
-					{5, 1, 9, 7, 2, 8, 6, 3, 4},
-					{8, 3, 7, 6, 1, 4, 2, 9, 5},
-					{1, 4, 3, 8, 6, 5, 7, 2, 9},
-					{9, 5, 8, 2, 4, 7, 3, 6, 1},
-					{7, 6, 2, 3, 9, 1, 4, 5, 8},
-					{3, 7, 1, 9, 5, 6, 8, 4, 2},
-					{4, 9, 6, 1, 8, 2, 5, 7, 3},
-					{2, 8, 5, 4, 7, 3, 9, 1, 6}};
+int sudoku[9][9];
+/*
+{{6, 2, 4, 5, 3, 9, 1, 8, 7}, 
+{5, 1, 9, 7, 2, 8, 6, 3, 4},
+{8, 3, 7, 6, 1, 4, 2, 9, 5},
+{1, 4, 3, 8, 6, 5, 7, 2, 9},
+{9, 5, 8, 2, 4, 7, 3, 6, 1},
+{7, 6, 2, 3, 9, 1, 4, 5, 8},
+{3, 7, 1, 9, 5, 6, 8, 4, 2},
+{4, 9, 6, 1, 8, 2, 5, 7, 3},
+{2, 8, 5, 4, 7, 3, 9, 1, 6}};
+*/
 
 typedef struct {
 	int line;
@@ -64,7 +67,26 @@ void *threadProcessBloc(void *arg) {
 	pthread_exit(EXIT_SUCCESS);
 }
 
+int reader() {
+	for (int i = 0; i < 9; i += 1) {
+		for (int j = 0; j < 9; j += 1) {
+			char c1, c2;
+			scanf("%c%c", &c1, &c2);
+			int number = c1 - '0';
+			if (number < 0 || number > 9)
+				return -1;
+			sudoku[i][j] = number;
+		}
+	}
+	return 0;
+}
+
 int main(int argc, char const *argv[]) {
+
+	if (reader() == -1) {
+		printf("reader(): Wrong format\n");
+		exit(EXIT_FAILURE);
+	}
 
 	int count = 0;	// Compteur des threads.
 	pthread_t tid[9*3];	// Tableau contenant les identifiants des différents threads.
