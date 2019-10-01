c73fc2e869173c0a603a1d580efc0c88ebad7189
Sevla <dasilvaalvesb@gmail.com>
2019-09-30 22:25:12 -0400

    cmake

---
+++ CMakeFiles/sudoku.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sevla/Projects/Sudoku-Validator/main.c" "/home/sevla/Projects/Sudoku-Validator/CMakeFiles/sudoku.dir/main.c.o"
+  "/home/sevla/Projects/Sudoku-Validator/reader.c" "/home/sevla/Projects/Sudoku-Validator/CMakeFiles/sudoku.dir/reader.c.o"
+  "/home/sevla/Projects/Sudoku-Validator/thread.c" "/home/sevla/Projects/Sudoku-Validator/CMakeFiles/sudoku.dir/thread.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
